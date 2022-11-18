158bb14cfa339339c78a255579d819de256ac28e
Shivank12batra <Shivank56batra@gmail.com>
2022-11-18 14:04:52 +0530

    minor css styling issues fixed

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -12,7 +12,7 @@ export default function Rules({setGameInfo}) {
             <h2>Guess the Wordle in 6 tries</h2>
             <p>• Each guess must be a 6-letter word.</p>
             <p>• The color of the tiles will change to show how close your guess was to the word</p>
-            <p>• Green: Correct Guess, Yellow: Current Guess, Wrong Position, Grey: Not In Wordle</p>
+            <p>• <span style={{'color':'#5ac85a', 'fontWeight':'bold'}}>Green:</span> Correct Guess, <span style={{'color':'#e2cc68', 'fontWeight':'bold'}}>Yellow</span>  Current Guess, Wrong Position, <span style={{'color':'#a1a1a1', 'fontWeight':'bold'}}>Grey:</span> Not In Wordle</p>
             <p>• Wordle is a 6 letter second name of a football player playing in the top 5 European Leagues.</p>
         </div>
     </div>
